1190616f386d08561e57fd4e66513e333b1305c4
muhsinalungal <muhsinalungal@gmail.com>
2024-11-09 17:22:45 +0300

    [ADD] backup module added

---
+++ auto_database_backup/__manifest__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2021-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': "Automatic Database Backup",
+    'version': '14.0.2.0.2',
+    'summary': 'Generate automatic backup of databases and store to local, '
+               'google drive or remote server',
+    'description': 'his module has been developed for creating '
+                   'database backups automatically '
+                   'and store it to the different locations.',
+    'author': "Cybrosys Techno Solutions",
+    'website': "https://www.cybrosys.com",
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'category': 'Tools',
+    'depends': ['base', 'mail', 'google_drive'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/data.xml',
+        'views/db_backup_configure_views.xml',
+        'wizard/dropbox_auth_code_views.xml'
+    ],
+    'external_dependencies': {
+        'python': ['dropbox', 'pyncclient', 'boto3', 'nextcloud-api-wrapper', 'paramiko']
+    },
+    'license': 'LGPL-3',
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ auto_database_backup/doc/RELEASE_NOTES.md
@@ -0,0 +1,31 @@
+## Module <auto_database_backup>
+
+#### 20.04.2022
+#### Version 14.0.1.0.0
+#### ADD
+- Initial commit for auto_database_backup
+
+#### 16.02.2024
+#### Version 14.0.1.0.1
+#### ADD
+- Dropbox integration added. Backup can be stored in to dropbox.
+
+#### 16.02.2024
+#### Version 14.0.1.0.1
+#### ADD
+- Onedrive integration added. Backup can be stored in to onedrive.
+
+#### 16.02.2024
+#### Version 14.0.1.0.1
+#### ADD
+- Google Drive authentication updated.
+
+#### 16.02.2024
+#### Version 14.0.1.0.1
+#### ADD
+- Nextcloud and Amazon S3 integration added. Backup can be stored into Nextcloud and Amazon S3.
+
+#### 24.10.2024
+#### Version 14.0.2.0.2
+#### UPDT
+-  Fixed the errors while inputting list_db = False in odoo conf file.
